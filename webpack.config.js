6c119d513f36be0920ca1e5cbc9dc2b41934ebaa
ajn008 <ajn008@bucknell.edu>
2018-02-07 19:32:36 -0500

    Initial commit, finished setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+var path = require("path");
+
+// copy everything that has been prepared into dist folder
+var DIST_DIR = path.resolve(__dirname, "dist");
+
+// copy from src folder
+var SRC_DIR = path.resolve(__dirname, "src");
+
+
+var config = {
+  entry: SRC_DIR + "/app/index.js",
+  output: {
+    path: DIST_DIR + "/app",
+    filename: "bundle.js",
+    publicPath: "/app/"
+  },
+  module:{
+    loaders: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015", "stage-2"]
+        }
+      }
+    ]
+  }
+
+};
+
+module.exports = config;
