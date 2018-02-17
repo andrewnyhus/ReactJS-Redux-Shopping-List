e9ff118a5ed2914d14132a8f1aaed46e873045e6
ajn008 <ajn008@bucknell.edu>
2018-02-17 02:57:25 -0500

    immutable create, update and delete shopping list item works programmatically, still needs GUI

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,27 +4,73 @@ import {Root} from "./Root";
 import {createStore} from "redux";
 
 
+
+
+
+
+
+
+
+// initialize initial state
+const initialState = {
+  result: {items: []},
+  lastValues: []
+};
+
+
 // set up reducer
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     case "CREATE":
       var value = action.payload.value;
 
-      state.items.push(value);
+      state = {
+        ...state,
+        result: {items: [...state.result.items, value]},
+        lastValues: [...state.lastValues, state.result]
+      };
+
       break;
 
     case "UPDATE":
       var indexToUpdate = action.payload.index;
       var newValue = action.payload.newValue;
 
-      state.items[indexToUpdate] = newValue;
+      state = {
+        ...state,
+        result: {
+          items:
+            state.result.items.map(
+              (item, index) => {
+                    if(index !== indexToUpdate) {
+                        // This isn't the item we care about - keep it as-is
+                        return item;
+                    }
+                    // Otherwise, this is the one we want - return an updated value
+                    return newValue;
+              })
+        },
+        lastValues: [...state.lastValues, state.result]
+      }
+
+
       break;
 
     case "DELETE":
       var indexToDelete = action.payload.index;
 
-      state.items.pop(indexToDelete);
+      state = {
+        ...state,
+        result: {
+          items:[
+            ...state.result.items.slice(0, indexToDelete),
+            ...state.result.items.slice(indexToDelete + 1)
+          ]
+        },
+        lastValues: [...state.lastValues, state.result]
+      }
+
       break;
   }
   return state;
@@ -32,39 +78,60 @@ const reducer = (state, action) => {
 
 
 // create store
-const store = createStore(reducer, {items:[]});
+const store = createStore(reducer);
 
-
-// subscribe to state changes
 store.subscribe(() => {
-  console.log("Store updated!", store.getState());
+  console.log("store updated: ");
+  console.log(store.getState());
 });
 
 
 store.dispatch({
   type: "CREATE",
-  payload: {value: "Example shopping item value"}
+  payload: {value: "Sample Value"}
+});
+store.dispatch({
+  type: "CREATE",
+  payload: {value: "Sample Value 2"}
 });
 
-/*store.dispatch({
+store.dispatch({
   type: "UPDATE",
-  payload: {index: 0, newValue: "Updated example value"}
+  payload: {newValue: "Updated Value", index: 0}
 });
 
 store.dispatch({
   type: "DELETE",
   payload: {index: 0}
-});*/
-
-
+});
 
 
 export class Home extends React.Component {
+
+
+
+  createShoppingItem(value){
+    store.dispatch({
+      type: "CREATE",
+      payload: {value: value}
+    });
+  }
+
+
   render(){
 
     const items = (
       <ul className={"list-group items_list"}>
+        {/*this.state.result.items.map((item, i) =>
 
+          <li className={"list-group-item shopping_list_item"}>
+            <button className="btn btn-primary">
+              <span class="glyphicon glyphicon-trash"></span>
+            </button>
+            {item}
+          </li>
+
+        )*/}
       </ul>
     );
 
